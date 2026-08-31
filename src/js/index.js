49d1ef05187a4a3d451a726e1d70d2ff287819d8
Letícia Cavadase <lecavadas45@gmail.com>
2026-08-31 16:59:33 -0300

    Adiciona Seguindo, Seguidores

--- src/js/index.js
+++ src/js/index.js
@@ -29,6 +29,17 @@ btnSearch.addEventListener("click", async () => {
                     <h2>${userData.name}</h2>
                     <p>${userData.bio || "Não possui bio cadastrada.😢"}</p>
                 </div>
+            </div>
+
+            <div class="profile-counters">
+                <div class= "followers">
+                    <h4>💕Seguidores</h4>
+                    <span>${userData.followers}</span>
+                </div>
+                <div class= "following">
+                    <h4>✨Seguindo</h4>
+                    <span>${userData.following}</span>
+                </div>
             </div>`;
 
 
